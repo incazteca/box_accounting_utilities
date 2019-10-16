319d5ac9b7f0576355d5fec2f1a75a203d30a0de
Franco Reyes <incazteca12345@gmail.com>
2019-10-15 22:38:15 -0500

    Refactor target transaction converter

--- lib/target_transaction_converter.rb
+++ lib/target_transaction_converter.rb
@@ -3,39 +3,18 @@
 require 'csv'
 require 'date'
 require 'bigdecimal'
+require_relative 'transaction_converter'
 
 # TargetTransactionConverter, used for converting transactions for Target CC to
 # a CSV we can use for box accounting
-class TargetTransactionConverter
-  US_DATE_FORMAT = '%m/%d/%Y'
-  UTC_DATE_FORMAT = '%Y-%m-%d'
-  OUTPUT_FILE_NAME = 'transactions.csv'
+class TargetTransactionConverter < TransactionConverter
   AMOUNT_REGEX = /\d+.\d{2}/.freeze
-
-  attr_reader :transactions, :input_filename
-
-  def initialize(filename)
-    @input_filename = filename
-    @transactions = []
-  end
-
-  def call
-    parse_transactions!
-    generate_output_csv
-  end
 
   def parse_transactions!
     CSV.foreach(@input_filename, converters: :numeric, headers: true) do |row|
       next if row['Reference Number'].nil?
 
       transactions << process_row(row)
-    end
-  end
-
-  def generate_output_csv
-    CSV.open(OUTPUT_FILE_NAME, 'wb') do |row|
-      row << transactions.first.keys
-      transactions.each { |t| row << t.values }
     end
   end
 
