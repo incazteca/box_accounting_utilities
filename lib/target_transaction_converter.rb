14cb90be4fd30ef19df29260983e84003f91ba2f
Franco Reyes <freyes1988@gmail.com>
2019-11-12 20:23:51 -0600

    WIP sorbet

--- lib/target_transaction_converter.rb
+++ lib/target_transaction_converter.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+# typed: true
 
 require 'csv'
 require 'date'
@@ -7,8 +8,12 @@ require_relative 'transaction_converter'
 # TargetTransactionConverter, used for converting transactions for Target CC to
 # a CSV we can use for box accounting
 class TargetTransactionConverter < TransactionConverter
+  # Bring the `sig` method into scope
+  extend T::Sig
+
   AMOUNT_REGEX = /\d+.\d{2}/.freeze
 
+  sig{returns(T::Array[T::Hash[String,String]])
   def parse_transactions!
     CSV.foreach(@input_filename, converters: :numeric, headers: true) do |row|
       next if row['Reference Number'].nil?
@@ -19,6 +24,7 @@ class TargetTransactionConverter < TransactionConverter
 
   private
 
+  sig {params(row: CSV::Row).returns(T::Hash[String, String])}
   def process_row(row)
     transaction_date = Date.strptime(row['Trans Date'], US_DATE_FORMAT)
     amount_parsed = AMOUNT_REGEX.match(row['Amount'])[0].to_f
